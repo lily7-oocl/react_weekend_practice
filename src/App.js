974f0e0a9309a7a211f793fefaddb461e00dbccd
lyle.li <lily7@oocl.com>
2025-09-12 14:57:47 +0800

    feat: add list to display

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { useReducer } from "react";
+import {useReducer} from "react";
 import './App.css';
 import TodoList from "./components/TodoList";
-import { initialState, todoReducer } from "./reducers/todoReducer";
-import { TodoContext } from "./contexts/TodoContext";
+import {initialState, todoReducer} from "./reducers/todoReducer";
+import {TodoContext} from "./contexts/TodoContext";
 
 function App() {
-  // the Hooks API manage component data state
-  const [state, dispatch] = useReducer(todoReducer, initialState);
-
-  return (
-    <div className="App">
-      <TodoContext.Provider value={{state, dispatch}}>
-        <TodoList/>
-      </TodoContext.Provider>
-    </div>
-  );
+    // the Hooks API manage component data state
+    const [state, dispatch] = useReducer(todoReducer, initialState);
+    const value = {state, dispatch};
+    return (
+        <div className="App">
+            <TodoContext.Provider value={value}>
+                <TodoList/>
+            </TodoContext.Provider>
+        </div>
+    );
 }
 
 export default App;
