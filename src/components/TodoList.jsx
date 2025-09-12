1aafeba9bd5869e1a67307323f23d87d0c717395
lyle.li <lily7@oocl.com>
2025-09-12 15:08:20 +0800

    feat: add css for TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,19 @@
 import {useContext} from "react";
 import {TodoContext} from "../contexts/TodoContext";
+import './TodoList.css';
 
 const TodoList = () => {
-  const {state, dispatch} = useContext(TodoContext)
-  return (
-      <div>
-        <div>This is the TodoList Component.</div>
-        {
-          state.map(todo => {
-            return <div>{todo.text}</div>
-          })
-        }
-      </div>
-  );
+    const {state, dispatch} = useContext(TodoContext)
+    return (
+        <div className={'todo-group'}>
+            <div>This is the TodoList Component.</div>
+            {
+                state.map(todo => {
+                    return <div className={'todo-item'}>{todo.text}</div>
+                })
+            }
+        </div>
+    );
 }
 
 export default TodoList
