7cd98baf44d09da777e54321166924ea86045a69
lyle.li <lily7@oocl.com>
2025-09-14 10:10:35 +0800

    feat: complete Click todo text can make it done with line through, click again to make it undone

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,12 +4,20 @@ import './TodoList.css';
 
 const TodoList = () => {
     const {state, dispatch} = useContext(TodoContext)
+
+    function toggleDone(id) {
+        const action = {type: 'DONE', id: id}
+        dispatch(action)
+    }
+
     return (
         <div className={'todo-group'}>
             <div>This is the TodoList Component.</div>
             {
-                state.map(todo => {
-                    return <div className={'todo-item'}>{todo.text}</div>
+                state.map(({text, done, id}) => {
+                    console.log("render todo:", text, done)
+                    return <div className={`todo-item ${done ? 'done' : ''}`}
+                                onClick={() => toggleDone(id)}>{text}</div>
                 })
             }
         </div>
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,9 +1,19 @@
 export const initialState = [
-  {id: 1, text: "the first todo", done: false},
-  {id: 2, text: "the second todo", done: false},
+    {id: 1, text: "the first todo", done: false},
+    {id: 2, text: "the second todo", done: false},
 ];
 
 // reducer is a pure function that define and gather all state update logic
 export const todoReducer = (state, action) => {
-  return state;
+    switch (action.type) {
+        case 'DONE':
+            return state.map((todo) => {
+                    if (todo.id === action.id) {
+                        return {...todo, done: !todo.done};
+                    }
+                    return todo;
+                }
+            );
+    }
+    return state;
 };
